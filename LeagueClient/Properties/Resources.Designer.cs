c954d4957c3feb56d65c1287f1b8f2a9da7784f1
Max Froehlich <maxwell.froehlich@gmail.com>
2016-02-23 22:46:42 -0500

    Added league icon to project

--- LeagueClient/Properties/Resources.Designer.cs
+++ LeagueClient/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace LeagueClient.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap LeagueIcon {
+            get {
+                object obj = ResourceManager.GetObject("LeagueIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap MatchHistoryIcon {
             get {
                 object obj = ResourceManager.GetObject("MatchHistoryIcon", resourceCulture);
